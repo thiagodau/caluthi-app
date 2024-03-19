0243be4d5a38655e414d3a2f3158a88a3d59c03f
Thiago <thiagorodriguesdau@gmail.com>
2024-03-19 15:51:41 -0400

    Added Login Validation and Navigate to Dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,55 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../firebaseConfig";
+
+import { getDatabase, ref, get, child } from "firebase/database";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const acessar = () => {
-    let user = { email, password }
-		console.log(user)
+    if(email && password) {
+      let user = { email, password };
+      validacaoUsuario(user);
+    }else {
+      alert('Campos vazios.')
+    }
   };
+
+  async function validacaoUsuario({ email, password }) {
+    console.log("buscando usuario...");
+
+    const database = ref(getDatabase());
+
+    await get(child(database, `users/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          let usuarios = snapshot.val();
+          let arr = Object.values(usuarios);
+          let usuarioEncontrado = arr.filter(
+            (user) => user.email == email && user.password == password
+          );
+
+          if (usuarioEncontrado.length == 1) {
+            localStorage.setItem("@user-id", usuarioEncontrado[0].id);
+            localStorage.setItem("@user-email", usuarioEncontrado[0].email);
+            navigate("/dashboard");
+          } else {
+            alert(
+              `Usuário não encontrato, verifique e-mail e senha ou cadastre-se.`
+            );
+          }
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error("erro aqui: " + error);
+      });
+  }
 
   return (
     <>
